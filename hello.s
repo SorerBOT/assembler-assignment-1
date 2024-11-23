bdce1a8d6d7594b77dab8a58405b9958ae8b6b4f
SorerBOT <alonfiller@gmail.com>
2024-11-23 20:50:38 +0200

    (Feat) works

--- hello.s
+++ hello.s
@@ -5,9 +5,13 @@ current_rnd_number:
 	.quad 0
 user_guess:
 	.quad 0
+rounds_won:
+	.quad 0
 is_easy_mode_char:
 	.byte 0
 space_eater_char:
+	.byte 0
+is_double_or_nothing_char:
 	.byte 0
 # miscellaneous
 N:
@@ -25,12 +29,10 @@ what_is_your_guess_str:
 	.string "What is your guess? "
 incorrect_str:
 	.string "Incorrect. "
-incorrect_str_EOL:
-	.string "Incorrect.\n"
 game_over_lost_str:
-	.string "Game over, you lost :(. The correct answer was "
+	.string "Incorrect.\nGame over, you lost :(. The correct answer was %ld\n"
 game_over_win_str:
-	.string "Congratz! You won 1 rounds!"
+	.string "Congratz! You won %ld rounds!\n"
 double_or_nothing_str:
 	.string "Double or nothing! Would you like to continue to another round? (y/n) "
 # formats
@@ -42,7 +44,9 @@ long_fmt:
 	.string "%d"
 char_fmt:
 	.string "%c"
-
+# constants
+yes_variable:
+	.byte 'y'
 .section .text
 .globl  main
 .type	main, @function
@@ -75,27 +79,17 @@ main:
 	leaq	is_easy_mode_char(%rip), %rsi
 	xorq	%rax, %rax
 	call	scanf
-	# Get Random Number Between 1 and N
+	# initialise current_random_number
+	call	initialise_current_random_number
+	# play round
 	xorq	%rax, %rax
-	call	get_rand_number_under_N
-	movq	%rax, current_rnd_number(%rip)
-	# Prompting User to Guess
-	movq	$what_is_your_guess_str, %rdi
-	xorq	%rax, %rax
-	call	printf
-	# Reading User Guess
-	movq	$quad_fmt, %rdi
-	leaq	user_guess(%rip), %rsi
-	xorq	%rax, %rax
-	call	scanf
-
+	call	play_game
 	# test
-	movq	$quad_fmt, %rdi
-	movq	current_rnd_number(%rip), %rsi
-	xorq	%rax, %rax
-	call	printf
+	# movq	$quad_fmt, %rdi
+	# movq	%rax, %rsi
+	# xorq	%rax, %rax
+	# call	printf
 
-	xorq	%rax, %rax
 	movq	%rbp, %rsp
 	popq	%rbp
 	ret
@@ -160,6 +154,145 @@ get_rand_number_under_N:
 	# Increment by ONE
 	incq	%rax
 	# Epilogue
+	movq	%rbp, %rsp
+	popq	%rbp
+	ret
+
+.globl	play_game
+.type	play_game, @function
+play_game:
+	# prologue
+	pushq	%rbp
+	movq	%rsp, %rbp
+
+	game_loop:
+		cmpq	$0, M(%rip)
+		je		game_over_lose
+		jl		game_over_win
+		jg		another_guess
+
+	game_over_lose:
+		movq	$game_over_lost_str, %rdi
+		movq	current_rnd_number, %rsi
+		call	printf
+		jmp		epilogue
+
+	game_over_win:
+		movq	$game_over_win_str, %rdi
+		movq	rounds_won(%rip), %rsi
+		call	printf
+		jmp		epilogue
+	another_guess:
+		call	play_guess
+		jmp		game_loop
+
+	# epilogue
+	epilogue:
+		movq	%rbp, %rsp
+		popq	%rbp
+		ret
+
+.globl	play_guess
+.type	play_guess, @function
+play_guess:
+	# prologue
+	pushq	%rbp
+	movq	%rsp, %rbp
+
+	# decrement M
+	decq	M(%rip)
+	# Prompting User to Guess
+	movq	$what_is_your_guess_str, %rdi
+	xorq	%rax, %rax
+	call	printf
+	# Reading User Guess
+	movq	$quad_fmt, %rdi
+	leaq	user_guess(%rip), %rsi
+	xorq	%rax, %rax
+	call	scanf
+
+	movq	current_rnd_number(%rip), %rdi
+	movq	user_guess(%rip), %rsi
+
+	cmpq	%rdi, %rsi
+	je		equal_case
+	jmp		not_equal_case
+
+	equal_case:
+		incq	rounds_won
+		call	double_or_nothing
+		jmp		epilogue_1
+
+	not_equal_case:
+		cmpq	$0, M(%rip)
+		je		epilogue_1
+		movq	$incorrect_str, %rdi
+		call	printf
+
+	# epilogue
+	epilogue_1:
+		movq	%rbp, %rsp
+		popq	%rbp
+		ret
+
+.globl double_or_nothing
+.type  double_or_nothing, @function
+double_or_nothing:
+	# prologue
+	pushq	%rbp
+	movq	%rsp, %rbp
+
+	movq	$double_or_nothing_str, %rdi
+	call	printf
+
+	# eat space
+	call	eat_space
+	# scan result
+	movq	$char_fmt, %rdi
+	leaq	is_double_or_nothing_char(%rip), %rsi
+	call	scanf
+	# compare
+	movb	is_double_or_nothing_char(%rip), %al
+	cmpb	$'y', %al
+	je		yes
+	jmp		no
+
+	yes:
+		movq	$5, M(%rip)
+		# multiply N
+		movq	N(%rip), %rax
+		movq	$2, %rbx
+		mulq	%rbx
+		movq	%rax, N(%rip)
+		# multiply seed
+		movq	user_seed(%rip), %rax
+		movq	$2, %rbx
+		mulq	%rbx
+		movq	%rax, user_seed(%rip)
+		# generate new current_random_number
+		call	initialise_current_random_number
+		jmp		epilogue_3
+	no:
+		movq	$-1, M(%rip)
+
+	epilogue_3:
+		movq	%rbp, %rsp
+		popq	%rbp
+		ret
+
+.globl initialise_current_random_number
+.type  initialise_current_random_number, @function
+initialise_current_random_number:
+	# prologue
+	pushq	%rbp
+	movq	%rsp, %rbp
+	# Get Random Number Between 1 and N
+	xorq	%rax, %rax
+	call	get_rand_number_under_N
+	movq	%rax, current_rnd_number(%rip)
+	# reset %rax
+	xorq	%rax, %rax
+	# epilogue
 	movq	%rbp, %rsp
 	popq	%rbp
 	ret
